ef0608acdd0d7f3370dd97b4b961af983b64aeff
callcocam <contato@sigasmart.com.br>
2022-09-14 23:03:48 -0300

    search

--- resources/views/components/form/search.blade.php
+++ resources/views/components/form/search.blade.php
@@ -8,13 +8,14 @@
             }
         }">
             <div class="relative flex flex-grow items-stretch focus-within:z-10">
-                <input class="block w-full  rounded-none rounded-l-md border-gray-300 pl-2 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                    type="{{ $type }}" id="{{ $id }}" readonly  value="{{ data_get($data, sprintf('data.%s', $modelName)) }}" 
-                    placeholder="{{ $selectedLabel }}"/>
+                <input
+                    class="block w-full  rounded-none rounded-l-md border-gray-300 pl-2 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                    type="{{ $type }}" id="{{ $id }}" readonly
+                    value="{{ data_get($data, sprintf('data.%s', $modelName)) }}" placeholder="{{ $selectedLabel }}" />
             </div>
             <button x-on:click="toggle" type="button"
                 class="relative -ml-px flex items-center space-x-2 rounded-r-md border border-gray-300 bg-gray-50 px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-100 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500">
-               <x-dynamic-component :component="Ui::component('icon')" :name="$rightIcon" class="{{ $iconSize }} shrink-0" />
+                <x-dynamic-component :component="Ui::component('icon')" :name="$rightIcon" class="{{ $iconSize }} shrink-0" />
             </button>
             <x-tall::search>
                 <div x-cloak @click.away="open=false" @close.stop="open=false"
@@ -32,35 +33,41 @@
                         <!-- Heroicon name: outline/plus -->
                         <x-tall::icons.outline.x class="h-4 w-4 shrink-0" />
                     </button>
-                    <!-- Results, show/hide based on command palette state -->
-                    <ul id="simplesidebar" class="max-h-72 py-2 text-sm overscroll-contain text-gray-800 p-2 "
-                        id="options" role="listbox">
-                        <!-- Active: "bg-indigo-600 text-white" -->
-                        @if ($options)
-                            @foreach ($options as $optLabel => $optValue)
-                                <li {{-- :class="{ 'bg-indigo-600 text-white': list.id == {{ data_get($data, $field) }} }" --}}
-                                    @if ($optValue == data_get($data, $name)) class="flex select-none px-4 py-2 cursor-default relative bg-indigo-500 text-white"
-                                    @else
-                                    class="flex select-none px-4 relative hover:bg-indigo-500 hover:text-white border-b" @endif
-                                    id="{{ $optValue }}" role="option">
-                                    <label for="{{$name }}.{{$optValue}}" class=" py-2 cursor-pointer w-full flex">
-                                    <span class="block truncate">{{ $optLabel }}</span>
-                                    <span class="absolute inset-y-0 right-0 flex items-center pr-4 text-indigo-600">
-                                        <!-- Heroicon name: mini/check -->
-                                        @if ($optValue == data_get($data, $name))
-                                            <x-tall::icons.outline.check class="h-5 w-5 text-white" />
-                                        @endif
-                                    </span>
-                                    <input class="hidden" value="{{$optValue}}"  wire:model="{{ $key }}" type="radio" name="{{ $name }}" id="{{$name }}.{{$optValue}}">
-                                </label>
-                                </li>
-                            @endforeach
+                    @isset($options)
+                        <ul id="simplesidebar" class="max-h-72 py-2 text-sm overscroll-contain text-gray-800 p-2 "
+                            id="options" role="listbox">
+                            <!-- Active: "bg-indigo-600 text-white" -->
+                            @if ($options)
+                                @foreach ($options as $optLabel => $optValue)
+                                    <li {{-- :class="{ 'bg-indigo-600 text-white': list.id == {{ data_get($data, $field) }} }" --}}
+                                        @if ($optValue == data_get($data, $name)) class="flex select-none px-4 py-2 cursor-default relative bg-indigo-500 text-white"
+                                @else
+                                class="flex select-none px-4 relative hover:bg-indigo-500 hover:text-white border-b" @endif
+                                        id="{{ $optValue }}" role="option">
+                                        <label for="{{ $name }}.{{ $optValue }}"
+                                            class=" py-2 cursor-pointer w-full flex">
+                                            <span class="block truncate">{{ $optLabel }}</span>
+                                            <span class="absolute inset-y-0 right-0 flex items-center pr-4 text-indigo-600">
+                                                <!-- Heroicon name: mini/check -->
+                                                @if ($optValue == data_get($data, $name))
+                                                    <x-tall::icons.outline.check class="h-5 w-5 text-white" />
+                                                @endif
+                                            </span>
+                                            <input class="hidden" value="{{ $optValue }}"
+                                                wire:model="{{ $key }}" type="radio" name="{{ $name }}"
+                                                id="{{ $name }}.{{ $optValue }}">
+                                        </label>
+                                    </li>
+                                @endforeach
+                            @endif
+                        </ul>
+                        @if (!count($options))
+                            <!-- Empty state, show/hide based on command palette state -->
+                            <p x-show="!lists.length" class="p-4 text-sm text-gray-500">{{ __('No people found.') }}</p>
                         @endif
-                    </ul>
-                    @if (!count($options))
-                        <!-- Empty state, show/hide based on command palette state -->
-                        <p x-show="!lists.length" class="p-4 text-sm text-gray-500">{{ __('No people found.') }}</p>
-                    @endif
+                    @endisset
+                    <!-- Results, show/hide based on command palette state -->
+
                 </div>
             </x-tall::search>
         </div>
