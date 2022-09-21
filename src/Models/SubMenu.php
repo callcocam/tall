4ad8a0f023976b4fb23a190a6100bae067dd7cfe
callcocam <contato@sigasmart.com.br>
2022-09-21 18:03:28 -0300

    atualizando tudo

--- src/Models/SubMenu.php
+++ src/Models/SubMenu.php
@@ -14,50 +14,6 @@ class SubMenu extends AbstractModel
     use HasFactory,UsesLandlordConnection;
 
     protected $guarded = ['id'];
-
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'created_at' => 'datetime:Y-m-d',
-        'updated_at' => 'datetime:Y-m-d'
-    ];
-     /**
-     * The accessors to append to the model's array form.
-     *
-     * @var array
-     */
-    // protected $appends = [
-    //     'parents'
-    // ];
-
-    //protected $table = "table";
-    public function sub_menus()
-    {
-        return $this->hasMany(\App\Models\SubMenu::class, 'sub_menu_id')
-        ->where('status', 'published')
-        ->orderby('ordering')
-        ->orderby('name');
-    }
-    <?php
-/**
-* Created by Bengs.
-* User: contato@bengs.com.br
-* https://www.bengs.com.br
-*/
-namespace Tall\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Tall\Tenant\Models\Concerns\UsesLandlordConnection;
-
-class Menu extends AbstractModel
-{
-    use HasFactory,UsesLandlordConnection;
-
-    protected $guarded = ['id'];
     protected $with = ['sub_menus'];
 
     //protected $table = "table";
@@ -83,11 +39,6 @@ class Menu extends AbstractModel
             $this->belongsTo(\App\Models\Menu::class, 'menu_id');
         }
         return $this->belongsTo(\Tall\Models\Menu::class, 'menu_id');
-    }
-
-    public function sub_menu()
-    {
-        return $this->belongsTo(\App\Models\SubMenu::class, 'sub_menu_id');
     }
 
     public function getParentsAttribute()
