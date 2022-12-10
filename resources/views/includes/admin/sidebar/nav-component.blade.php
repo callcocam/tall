a98723d9e4d574a09a8b20df38612b3e663c0585
bengsinc <111383298+bengsinc@users.noreply.github.com>
2022-12-10 10:05:39 -0300

    Link dashboard por default na sidebar

--- resources/views/includes/admin/sidebar/nav-component.blade.php
+++ resources/views/includes/admin/sidebar/nav-component.blade.php
@@ -48,7 +48,15 @@
             <!-- Navigation -->
             <div x-data="{}" x-init="new SimpleBar($el)">
                 <nav class="mt-6 px-3 max-h-[400px] overflow-auto">
-
+                    <div class="space-y-1">
+                        <!-- Current: "bg-gray-200 text-gray-900", Default: "text-gray-700 hover:text-gray-900 hover:bg-gray-50" -->
+                        <a href="{{ route('dashboard') }}" class=" hover:text-gray-100 hover:bg-indigo-800  group flex items-center px-2 py-2 text-sm font-medium rounded-md text-gray-200" aria-current="page">
+                            <svg class="mr-1 h-5 w-4 flex-shrink-0" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path>
+                            </svg>
+                            <span> Dashboard</span>
+                        </a>
+                    </div>
                     @includeIf('tall::fluxos-nav')
                     @if ($menus = $this->menus)
                         @foreach ($menus as $menu)
